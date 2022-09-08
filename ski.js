bda6030127e7dad0c9343d54073ef2a4748661b9
ski <108815730+thelegendski@users.noreply.github.com>
2022-09-07 20:49:11 -0400

    Added version and revised Polar's credit

--- ski.js
+++ ski.js
@@ -1,6 +1,6 @@
 /*
 ski.js
-version 1.4.5
+version 1.4.6
 
 Copyright 2022 thelegendski
 
@@ -26,7 +26,7 @@ credits:
 	PT - khanacademy.org/profile/kaid_1007254215278267494550608 - helping to incorporate requestAnimationFrame over setInterval as well for the structure of the get function
 	Processing.js - the use of the radius parameter in the rect function an' its related code
 	bravo bravo - khanacademy.org/profile/kaid_3550040499775075061747637 - advising the use of ctx.getImageData for the get function
-	Polar - khanacademy.org/profile/kaid_80710011086149831327935- for the drawImage an' getImage functions which have been modified
+	Polar - khanacademy.org/profile/kaid_80710011086149831327935- for the async image method an' getImage function which have been modified
 */
 
 //all variables at global scope
