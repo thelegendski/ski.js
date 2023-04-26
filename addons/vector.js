d25f35b5a59e90d7be4ef736a47fcfb78a58e26d
ski <108815730+thelegendski@users.noreply.github.com>
2023-04-26 08:53:16 -0400

    Update vector.js

--- addons/vector.js
+++ addons/vector.js
@@ -8,11 +8,10 @@ class Vector {
 		!y ? this.set(v.x || v[0] || 0, v.y || v[1] || 0, v.z || v[2] || 0) : (this.x = v || 0, this.y = y || 0, this.z = z || 0)
 	}
 	get() {
-		return new vec(this.x,this.y,this.z)
+		return new Vector(this.x,this.y,this.z)
 	}
 	mag() {
-		const S = Math.sqrt
-		return S(this.x ** 2 + this.y ** 2 + this.z ** 2)
+		return Math.sqrt(this.x ** 2 + this.y ** 2 + this.z ** 2)
 	}
 	magSq() {
 		return (this.x ** 2 + this.y ** 2 + this.z ** 2)
@@ -46,14 +45,13 @@ class Vector {
 		this.y] = [c(d(ang)) * this.x - s(d(ang)) * this.y, s(d(ang)) * this.x + c(d(ang)) * this.y]
 	}
 	dist(v) {
-		const S = Math.sqrt
-		return S(((this.x - v.x) * (this.x - v.x)) + ((this.y - v.y) * (this.y - v.y)) + ((this.z - v.z) * (this.z - v.z)))
+		return Math.sqrt(((this.x - v.x) * (this.x - v.x)) + ((this.y - v.y) * (this.y - v.y)) + ((this.z - v.z) * (this.z - v.z)))
 	}
 	dot(v, y=false, z) {
 		return !y ? (this.x * v.x + this.y * v.y + this.z * v.z) : (this.x * v + this.y * y + this.z * z)
 	}
 	cross(v) {
-		return new vec(this.y * v.z - v.y * this.z,this.z * v.x - v.z * this.x,this.x * v.y - v.x * this.y)
+		return new Vector(this.y * v.z - v.y * this.z,this.z * v.x - v.z * this.x,this.x * v.y - v.x * this.y)
 	}
 	lerp(v, A, z=false, amt) {
 		function l (v, t, a) {
@@ -69,7 +67,7 @@ class Vector {
 		this.mult(high))
 	}
 	heading() {
-		return t(-this.y, this.x)
+		return -Math.atan2(-this.y, this.x)
 	}
 	heading2d() {
 		this.heading()
@@ -80,7 +78,7 @@ class Vector {
 	toString() {
 		return this.array().toString()
 	}
-	static fromAngle(ang, v=new vec(), m=1) {
+	static fromAngle(ang, v=new Vector(), m=1) {
 		const c = Math.cos
 		  , s = Math.sin
 		  , d = ang=>ang * (Math.PI / 180)
@@ -90,7 +88,7 @@ class Vector {
 	}
 	static random2d(v) {
 		const r = Math.random
-		return vec.fromAngle(r() * 360, v)
+		return Vector.fromAngle(r() * 360, v)
 	}
 	static random3d(v=false) {
 		const r = Math.random
@@ -102,18 +100,18 @@ class Vector {
 		  , m = S(1 - z ** 2)
 		  , x = m * c(ang)
 		  , y = m * s(ang)
-		!v ? (v = new vec(x,y,z)) : v.set(x, y, z)
+		!v ? (v = new Vector(x,y,z)) : v.set(x, y, z)
 		return v
 	}
 	static sub(v, V) {
-		return new vec(v.x - V.x,v.y - V.y,!v.z && !V.z ? 0 : v.z - V.z)
+		return new Vector(v.x - V.x,v.y - V.y,!v.z && !V.z ? 0 : v.z - V.z)
 	}
 	static angleBetween(v, V) {
 		const a = Math.acos
 		return a(v.dot(V) / (v.mag() * V.mag()))
 	}
 	static lerp(v, V, A) {
-		v = new vec(v.x,v.y,v.z)
+		v = new Vector(v.x,v.y,v.z)
 		v.lerp(V, A)
 		return v
 	}
