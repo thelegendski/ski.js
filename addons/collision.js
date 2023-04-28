bb743e25dfbcc25baf8faaa80f9057f59ccefb74
ski <108815730+thelegendski@users.noreply.github.com>
2023-04-28 08:27:05 -0400

    Update collision.js

--- addons/collision.js
+++ addons/collision.js
@@ -1,84 +1,193 @@
-var col = {
-	//rect-circle physics
-	rcp (a,b,s,m=1) {
-		const x = constrain(a.x, m ? b.x - (b.width ?? b.w) / 2 : b.x, m ? b.x + (b.width ?? b.w) / 2 : b.x + (b.width ?? b.w)), y = constrain(a.y, m ? b.y - (b.height ?? b.h) / 2 : b.y, m ? b.y + (b.height ?? b.h) / 2 : b.y + (b.height ?? b.h))
-		s = s ?? a.size / 2
-		if (dist(a.x, a.y, x, y) <= s) {
-			const ang = atan2(a.x - x, a.y - y)
-			return [x + sin(ang) * s, y + cos(ang) * s]
-		}
+//collision.js has a dependency of main.js
+/**
+ * the collision object
+ * @namespace
+**/
+var collision = {
+    /**
+     * returns the euclidean distance of two coordinates squared
+     * @param {number} x1
+     * @param {number} y1
+     * @param {number} x2
+     * @param {number} y2
+     * @returns {number} - the distance
+    **/
+    dist (x1, y1, x2, y2) {
+      return (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)
+    },
+	rectCircle: {
+	    /**
+	     * returns whether a circle and a rectangle are colliding
+         * @param {{x: number, y: number, size: number}} circle
+         * @param {{x: number, y: number, width: number, height: number}} rectangle
+         * @param {number} [size=circle.size] - size of the circle for collisions
+         * @returns {boolean}
+	    **/
+        collide (circle, rect, size = circle.size) {
+            let x, y
+            rect.width = rect.width ?? rect.w
+            rect.height = rect.height ?? rect.h
+            size = circle.size ?? circle.s ?? circle.radius ?? circle.r ?? circle.diameter / 2 ?? circle.d / 2
+            if(skiJSData.rect){
+                x = constrain(circle.x, rect.x - rect.width / 2, rect.x + rect.width / 2)
+                y = constrain(circle.y, rect.y - rect.height / 2, rect.y + rect.height / 2)
+            }
+            else {
+                x = constrain(circle.x, min(rect.x, rect.x + rect.width), max(rect.x, rect.x + rect.width))
+                y = constrain(circle.y, min(rect.y, rect.y + rect.height), max(rect.y, rect.y + rect.height))
+            }
+            return collision.dist(circle.x, circle.y, x, y) <= sq(size)
+        },
+        /**
+         * returns the resolution position of a circle when in collision with a rectangle
+         * @param {{x: number, y: number, size: number}} circle
+         * @param {{x: number, y: number, width: number, height: number}} rectangle
+         * @param {number} [size=circle.size] - size of the circle for collisions
+         * @returns {{x: number, y: number}}
+        **/
+        physics (circle, rect, size = circle.size) {
+            let x, y
+            rect.width = rect.width ?? rect.w
+            rect.height = rect.height ?? rect.h
+            size = circle.size ?? circle.s ?? circle.radius ?? circle.r ?? circle.diameter / 2 ?? circle.d / 2
+            size /= 2
+            if(skiJSData.rect === CENTER){
+                x = constrain(circle.x, rect.x - rect.width / 2, rect.x + rect.width / 2)
+                y = constrain(circle.y, rect.y - rect.height / 2, rect.y + rect.height / 2)
+            }
+            else {
+                x = constrain(circle.x, min(rect.x, rect.x + rect.width), max(rect.x, rect.x + rect.width))
+                y = constrain(circle.y, min(rect.y, rect.y + rect.height), max(rect.y, rect.y + rect.height))
+            }
+            if(collision.dist(circle.x, circle.y, x, y) <= sq(size)){
+                const ang = atan2(circle.x - x, circle.y - y)
+                return {x: x + sin(ang) * size, y: y + cos(ang) * size}
+            }
+        },
 	},
-	//circle-circle physics
-	ccp (a,b,s) {
-		s = s ?? (a.size ?? a.s + b.size ?? b.s) / 2
-		if (dist(a.x, a.y, b.x, b.y) <= s) {
-			const ang = atan2(a.y - b.y, a.x - b.x)
-			return [b.x + cos(ang) * s, b.y + sin(ang) * s]
-		}
-	},
-	//circle-circle collisions
-	cc: (a,b,s)=>dist(a.x, a.y, b.x, b.y) <= (s ?? (a.size ?? a.s + b.size ?? b.s)) / 2,
-	//rect-circle collisions
-	rc: (a,b,m=1)=>dist(a.x, a.y, constrain(a.x, m ? b.x - b.width / 2 : min(b.x, b.x + b.width), m ? b.x + b.width / 2 : max(b.x, b.x + b.width)), constrain(a.y, m ? b.y - b.height / 2 : min(b.y, b.y + b.height), m ? b.y + b.height / 2 : max(b.y, b.y + b.height))) <= (a.size ?? a.s) / 2,
-	//line-line collisions
-	ll (a,b,c,d) {
-		const on = (a,b,c) => b.x <= max(a.x, c.x) && b.x >= min(a.x, c.x) && b.y <= max(a.y, c.y) && b.y >= min(a.y, c.y), rot = (a, b, c) => {const val = (b.x - a.x) * (c.y - b.y) - (b.y - a.y) * (c.x - b.x); return val > 0 ? 1 : val >= 0 ? 0 : 2}, o = [rot(a, b, c), rot(a, b, d), rot(c, d, a), rot(c, d, b)]
-		return (o[0] !== o[1] && o[2] !== o[3]) ||  (o[0] === 0 && on(a, c, b)) || (o[1] === 0 && on(a, d, b)) || (o[2] === 0 && on(c, a, d)) || (o[3] === 0 && on(c, b, d))
-	},
-	//line-line intersection
-	lli (a,b,c,d) {
-		if(!col.ll(a, b, c, d)) return
-		const s = (((b.x - a.x) * (a.y - c.y)) + ((b.y - a.y) * c.x) - ((b.y - a.y) * a.x)) / (((b.x - a.x) * (d.y - c.y)) - ((b.y - a.y) * (d.x - c.x)))
-		return {x: c.x + (d.x - c.x) * s, y: c.y + (d.y - c.y) * s}
-	},
-	//circle-line collisions [ol']
-	lc (a,b,c) {
-		!c.size && (c.size = c.s || c.r)
-		const rad = c.size / 2, ang = atan2(b.y - a.y, b.x - a.x) - 90, d = {x: c.x + cos(ang) * rad, y: c.y + sin(ang) * rad}, e = {x: c.x + cos(ang) * -rad, y: c.y + sin(ang) * -rad}, cache = [dist(a.x, a.y, c.x, c.y) <= rad, dist(b.x, b.y, c.x, c.y) <= rad], data = col.ll(a, b, d, e)
-		return data || cache[0] || cache[1]
-	},
-	//circle-line intersection [ol']
-	lci (a,b,c) {
-		!c.size && (c.size = c.s || c.r)
-		const rad = c.size / 2, ang = atan2(b.y - a.y, b.x - a.x) - 90, d = {x: c.x + cos(ang) * rad, y: c.y + sin(ang) * rad}, e = {x: c.x + cos(ang) * -rad, y: c.y + sin(ang) * -rad}, cache = [dist(a.x, a.y, c.x, c.y), dist(b.x, b.y, c.x, c.y)], data = col.ll(a, b, d, e);
-		return data ? {c: d, d: e, 0: rad} : cache[0] <= rad ? {int: a, 0: rad, 1: cache[0]} : cache[1] <= rad ? {int: b, 0: rad, 1: cache[1]} : false;
-	},
-	//circle-line physics [ol']
-	lcp (a,b,c) {
-		!c.size && (c.size = c.s || c.r)
-		const cache = col.lci(a, b, c)
-		if(!cache) return
-		const int = cache.int || col.lli(a, b, cache.c, cache.d), distance = (cache[1] || dist(int.x, int.y, c.x, c.y)) - cache[0], angle = atan2(int.y - c.y, int.x - c.x)
-		return [c.x + cos(angle) * distance, c.y + sin(angle) * distance]
-	},
-	//circle-line collision [new]
-	cpl (p, a, b) {
-		const d = {
-			x: p.x - a.x,
-			y: p.y - a.y
-		},
-		e = {
-			x: b.x - a.x,
-			y: b.y - a.y
-		},
-		s = constrain((d.x * e.x + d.y * e.y) / (e.x * e.x + e.y * e.y), 0, 1)
-
-		return [a.x + (e.x * s), a.y + (e.y * s)]
-	},
-	//circle-line physix [new]
-	clp (c, a, b, t=1) {
-		const closest = col.cpl(c, a, b),
-		d = {
-			x: c.x - closest[0],
-			y: c.y - closest[1]
-		}, 
-		DIST = sqrt(d.x * d.x + d.y * d.y),
-		o = DIST - t - ((c.s ?? c.size) / 2)
-
-		d.x /= DIST
-		d.y /= DIST
-
-		if(o <= 0) return [c.x - d.x * o, c.y - d.y * o]
-	},
+    circleCircle: {
+        /**
+	     * returns whether a circle and a circle are colliding
+         * @param {{x: number, y: number, size: number}} circle1
+         * @param {{x: number, y: number, size: number}} circle2
+         * @param {number} [size=circle.size] - avg. size of the circle for collisions
+         * @returns {boolean}
+	    **/
+        collide (circle1, circle2, size) {
+            size = circle1.size ?? circle1.s ?? circle1.radius ?? circle1.r ?? circle1.diameter / 2 ?? circle2.d / 2 + circle2.size ?? circle2.s ?? circle2.radius ?? circle2.r ?? circle2.diameter / 2 ?? circle2.d / 2
+            return collision.dist(circle1.x, circle1.y, circle2.x, circle2.y) <= sq(size)
+        },
+        /**
+         * returns the resolution position of a circle when in collision with a circle
+         * @param {{x: number, y: number, size: number}} circle
+         * @param {{x: number, y: number, size: number}} rectangle
+         * @param {number} [size=circle.size] - avg. size of the circle for collisions
+         * @returns {{x: number, y: number}}
+        **/
+        physics (circle1, circle2, size) {
+            size = circle1.size ?? circle1.s ?? circle1.radius ?? circle1.r ?? circle1.diameter / 2 ?? circle2.d / 2 + circle2.size ?? circle2.s ?? circle2.radius ?? circle2.r ?? circle2.diameter / 2 ?? circle2.d / 2
+            if(collision.dist(circle1.x, circle1.y, circle2.x, circle2.y) <= sq(size)){
+                const ang = atan2(circle1.y - circle2.y, circle1.x - circle2.x)
+                return {x: circle2.x + cos(ang) * size, y: circle2.y + sin(ang) * size}
+            }
+        },
+    },
+    lineLine: {
+        /**
+         * @ignore
+        **/
+        on (a, b, c) {
+            return b.x <= max(a.x, c.x) && b.x >= min(a.x, c.x) && b.y <= max(a.y, c.y) && b.y >= min(a.y, c.y)
+        },
+        /**
+         * @ignore
+        **/
+        rot (a, b, c) {
+            const val = (b.x - a.x) * (c.y - b.y) - (b.y - a.y) * (c.x - b.x) 
+            return val > 0 ? 1 : val >= 0 ? 0 : 2
+        },
+        /**
+         * returns true if two lines are colliding
+         * @param {{x: number, y: number}} a - first endpoint on the first line
+         * @param {{x: number, y: number}} b - second endpoint on the first line
+         * @param {{x: number, y: number}} c - first endpoint on the second line
+         * @param {{x: number, y: number}} d - second endpoint on the second line
+         * @returns {boolean}
+        **/
+        collide (a, b, c, d) {
+            const rot = this.rot, on = this.on
+            const arr = [rot(a, b, c), rot(a, b, d), rot(c, d, a), rot(c, d, b)]
+            return (arr[0] !== arr[1] && arr[2] !== arr[3]) || (arr[0] === 0 && on(a, c, b)) || (arr[1] === 0 && on(a, d, b)) || (arr[2] === 0 && on(c, a, d)) || (arr[3] === 0 && on(c, b, d))
+        },
+        /**
+         * returns the intersection point of two lines if they are intersecting
+         * @param {{x: number, y: number}} a - first endpoint on the first line
+         * @param {{x: number, y: number}} b - second endpoint on the first line
+         * @param {{x: number, y: number}} c - first endpoint on the second line
+         * @param {{x: number, y: number}} d - second endpoint on the second line
+         * @returns {{x: number, y: number}} - the intersection point
+        **/
+        intersect (a, b, c, d) {
+            if(!this.collide(a, b, c, d)) return
+            const val = (((b.x - a.x) * (a.y - c.y)) + ((b.y - a.y) * c.x) - ((b.y - a.y) * a.x)) / (((b.x - a.x) * (d.y - c.y)) - ((b.y - a.y) * (d.x - c.x)))
+            return {x: c.x + (d.x - c.x) * val, y: c.y + (d.y - c.y) * val}
+        },
+        /**
+         * given a target position, finds the closest point on a line
+         * @param {{x: number, y: number}} target
+         * @param {{x: number, y: number}} point1 - first endpoint on the line
+         * @param {{x: number, y: number}} point2 - second endpoint on the line
+         * @returns {{x: number, y: number}} - closest point
+        **/
+        closest (target, point1, point2) {
+            const d = {x: target.x - point1.x, y: target.y - point1.y}
+            const e = {x: point2.x - point1.x, y: point2.y - point1.y}
+            const s = constrain((d.x * e.x + d.y * e.y) / (e.x * e.x + e.y * e.y), 0, 1)
+            return {x: point1.x + (e.x * s), y: point1.y + (e.y * s)}
+        }
+    },
+    circleLine: {
+        /**
+         * returns true if a circle is colliding with a line
+         * @param {{x: number, y: number, size: number}} circle
+         * @param {{x: number, y: number}} point1 - first endpoint on the line
+         * @param {{x: number, y: number}} point2 - second endpoint on the line
+         * @param {number} [thickness=1] - the thickness of the line; stroke weight
+         * @returns {boolean}
+        **/
+        collide (circle, point1, point2, thickness = 1) {
+            const size = circle.size ?? circle.s ?? circle.radius ?? circle.r ?? circle.diameter / 2 ?? circle.d / 2
+            const closest = collision.lineLine.closest(circle, point1, point2)
+            const c = {x: circle.x - closest.x, y: circle.y - closest.y}
+            const distance = sqrt(c.x * c.x + c.y * c.y)
+            const o = distance - thickness - size / 2
+            
+            if(o <= 0) return true
+            else return false
+        },
+        /**
+         * returns the resolved position of a circle if the circle is colliding with a line
+         * @param {{x: number, y: number, size: number}} circle
+         * @param {{x: number, y: number}} point1 - first endpoint on the line
+         * @param {{x: number, y: number}} point2 - second endpoint on the line
+         * @param {number} [thickness=1] - the thickness of the line; stroke weight
+         * @returns {{x: number, y: number}} - the resolution coordinates
+        **/
+        physics (circle, point1, point2, thickness = 1) {
+            const size = circle.size ?? circle.s ?? circle.radius ?? circle.r ?? circle.diameter / 2 ?? circle.d / 2
+            const closest = collision.lineLine.closest(circle, point1, point2)
+            const c = {x: circle.x - closest.x, y: circle.y - closest.y}
+            const distance = sqrt(c.x * c.x + c.y * c.y)
+            const o = distance - thickness - size / 2
+            
+            c.x /= distance
+            c.y /= distance
+            
+            if(o <= 0){
+                return {x: circle.x - c.x * o, y: circle.y - c.y * o}
+            }
+        }
+    },
 }
-window.col = col
+Object.freeze(collision)
+window.collision = collision
